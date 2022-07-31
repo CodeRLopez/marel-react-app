4a2080ec4822ac10e8a8f209bad38f4b883bbc90
ronald <rlopez@nicarao.agency>
2022-07-31 13:40:46 -0600

    Agregue nombre a cada carde hice responsive

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   const [personajes, setPersonajes] = useState([]);
 
   useEffect(() => {
-    axios.get('lhttps://gateway.marvel.com:443/v1/public/characters?limit=6&ts=1&apikey=4f1c0b87d2c3832319457bd835ad0225&hash=8c5681ec8d747cda59a34afbb90155d7')
+    axios.get(`ihttps://gateway.marvel.com:443/v1/public/characters?nameStartsWith=hulk&limit=6&ts=1&apikey=4f1c0b87d2c3832319457bd835ad0225&hash=8c5681ec8d747cda59a34afbb90155d7`)
       .then(res => {
         setPersonajes(res.data.data.results)
       }).catch(error => console.error(error))
@@ -34,8 +34,24 @@ function App() {
         <div className="logo"></div>
       </header>
 
-      
+      <div className="card">
+        {personajes.map(per => {
+
+          if (personajes){
+            return (
+              <div className="card-item">
+                <img src={`${per.thumbnail.path}.${per.thumbnail.extension}`} alt=""></img>
+                <h2>{per.name}</h2>
+              </div>
+              )
+          } else (
+            <h1>Ingresa un nombre</h1>
+          )
         
+        })}
+      </div>
+
+
     </div>
   );
 }
