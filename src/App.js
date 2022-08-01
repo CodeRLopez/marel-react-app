7b0c9078a789901ff5f66011020223fcf51f1da4
ronald <rlopez@nicarao.agency>
2022-07-31 21:54:05 -0600

    Agregue informacion de la api como comics disponibles por personaje

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   const [personajes, setPersonajes] = useState([]);
 
   useEffect(() => {
-    axios.get(`ihttps://gateway.marvel.com:443/v1/public/characters?nameStartsWith=hulk&limit=6&ts=1&apikey=4f1c0b87d2c3832319457bd835ad0225&hash=8c5681ec8d747cda59a34afbb90155d7`)
+    axios.get(`phttps://gateway.marvel.com:443/v1/public/characters?nameStartsWith=hulk&limit=6&ts=1&apikey=4f1c0b87d2c3832319457bd835ad0225&hash=8c5681ec8d747cda59a34afbb90155d7`)
       .then(res => {
         setPersonajes(res.data.data.results)
       }).catch(error => console.error(error))
@@ -34,19 +34,31 @@ function App() {
         <div className="logo"></div>
       </header>
 
-      <div className="card">
+      {/* searchBar */}
+
+      <div className="cards">
         {personajes.map(per => {
 
-          if (personajes){
             return (
+              <div className="cards-container">
+
               <div className="card-item">
                 <img src={`${per.thumbnail.path}.${per.thumbnail.extension}`} alt=""></img>
+                <div className="card-info">
                 <h2>{per.name}</h2>
+                <h2>{`This character has ${per.comics.available} available comics`}</h2>
+                </div>
+              </div>
+
+              <div className="card-item2 none">
+                <img src={`${per.thumbnail.path}.${per.thumbnail.extension}`} alt=""></img>
+              </div>
+              <h2 className="name-hero none">{per.name}<br></br>{`Available comics: ${per.comics.available}`}</h2>
+              
+
+
               </div>
               )
-          } else (
-            <h1>Ingresa un nombre</h1>
-          )
         
         })}
       </div>
