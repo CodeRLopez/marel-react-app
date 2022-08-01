74f94f7f99d2af0022ada576e3316529685cf666
ronald <rlopez@nicarao.agency>
2022-08-01 01:46:24 -0600

    Cree input y funcion fetchHeroes para introducir en la url el dato introducido al input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, {useState } from "react";
 import "./App.css";
-import axios from "axios";
-
-
 
 //https://gateway.marvel.com:443/v1/public/characters?apikey=4f1c0b87d2c3832319457bd835ad0225
 
@@ -14,58 +11,60 @@ import axios from "axios";
 
 //hash: 8c5681ec8d747cda59a34afbb90155d7
 
-
 function App() {
 
   const [personajes, setPersonajes] = useState([]);
+  const [hero, setHero] = useState('')
 
-  useEffect(() => {
-    axios.get(`phttps://gateway.marvel.com:443/v1/public/characters?nameStartsWith=hulk&limit=6&ts=1&apikey=4f1c0b87d2c3832319457bd835ad0225&hash=8c5681ec8d747cda59a34afbb90155d7`)
-      .then(res => {
-        setPersonajes(res.data.data.results)
-      }).catch(error => console.error(error))
-  }, [])
+  const fetchHeroes = async (e) => {
+    e.preventDefault();
 
-  console.log(personajes)
+      const data = await fetch(`https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${hero}&limit=9&ts=1&apikey=4f1c0b87d2c3832319457bd835ad0225&hash=8c5681ec8d747cda59a34afbb90155d7`)
+      const res = await data.json()
+      setPersonajes(res.data.results)
+  }
 
-  return (
-    <div className="App">
-      <header className="header-app">
-        <div className="logo"></div>
-      </header>
 
-      {/* searchBar */}
+return (
+  <div className="App">
+    <header className="header-app">
+      <div className="logo"></div>
+    </header>
 
-      <div className="cards">
-        {personajes.map(per => {
+    <div className="search">
+      <form onSubmit={fetchHeroes}>
+        <input onChange={(e) => setHero(e.target.value)} placeholder="Super hero name..." />
+        <button className="btn" type="submit" >
+          Search
+        </button>
+      </form>
+    </div>
 
-            return (
-              <div className="cards-container">
+    <div className="cards" >
+      {personajes.map(per => {
 
-              <div className="card-item">
-                <img src={`${per.thumbnail.path}.${per.thumbnail.extension}`} alt=""></img>
-                <div className="card-info">
+        return (
+          <div className="cards-container" key={per.id}>
+
+            <div className="card-item" >
+              <img src={`${per.thumbnail.path}.${per.thumbnail.extension}`} alt=""></img>
+              <div className="card-info">
                 <h2>{per.name}</h2>
                 <h2>{`This character has ${per.comics.available} available comics`}</h2>
-                </div>
               </div>
+            </div>
 
-              <div className="card-item2 none">
-                <img src={`${per.thumbnail.path}.${per.thumbnail.extension}`} alt=""></img>
-              </div>
-              <h2 className="name-hero none">{per.name}<br></br>{`Available comics: ${per.comics.available}`}</h2>
-              
+            <div className="card-item2 none">
+              <img src={`${per.thumbnail.path}.${per.thumbnail.extension}`} alt=""></img>
+            </div>
+            <h2 className="name-hero none">{per.name}<br></br>{`Available comics: ${per.comics.available}`}</h2>
 
-
-              </div>
-              )
-        
-        })}
-      </div>
-
-
+          </div>
+        )
+      })}
     </div>
-  );
+  </div>
+);
 }
 
 export default App;
